51a2ad8d017ec49c604f8e5b9d1f64c7954c00a7
Danie231 <danielezra10@gmail.com>
2018-08-01 18:21:19 +0300

    project can now run video component, but need to change again

--- android/app/src/main/java/com/yea/MainApplication.java
+++ android/app/src/main/java/com/yea/MainApplication.java
@@ -3,6 +3,10 @@ package com.yea;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.corbt.keepawake.KCKeepAwakePackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +31,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
+            new OrientationPackage(),
+            new VectorIconsPackage(),
+            new KCKeepAwakePackage(),
             new ReactVideoPackage(),
             new ImagePickerPackage(),
             new A0Auth0Package()
